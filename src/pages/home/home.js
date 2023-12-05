4a976d71cefd6c490435c700ab35e00a8bc25227
ohunazz <ohunazz@gmail.com>
2023-12-05 08:08:37 -0500

    Add Hero Section

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,9 +1,11 @@
+import { Hero } from "./sections/hero/Hero";
 import { TopNavigation } from "./sections/top-navigation/TopNavigation";
 
 const Home = () => {
     return (
         <>
             <TopNavigation />
+            <Hero />
         </>
     );
 };
